f5d972a1e035a968494acf87ba1aa6b642ff37ce
Ayman <ayman.salah@2coom.com>
2024-06-30 16:19:14 +0300

    API For the store

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,27 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Store;
+use App\Models\Product;
+use Symfony\Component\HttpFoundation\Test\Constraint\ResponseFormatSame;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/store/{storeId}', function (Request $request) {
+    $theStoreId  = $request->storeId;
+    $theStore =  Store::find($theStoreId);
+    if ($theStore) {
+        $allStoreProducts  = $theStore->products()->get();
+        $filteredProducts = $allStoreProducts->map(function ($product) {
+            return $product->makeHidden(['created_at', 'updated_at', 'store_id'])->toArray();
+        });
+        return response()->json($filteredProducts);
+    } else {
+        return response()->json(['error' => 'Store not found'], 400);
+    }
 });
