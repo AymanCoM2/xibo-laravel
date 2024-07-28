9826fd3345af5fe5ff8c7c9c055bb8117ff17a21
ayman <ayman.s@kuwaitb2bservices.com>
2024-07-28 12:42:00 +0300

    Layout and Display truncate and Refrewh

--- routes/Xibo.php
+++ routes/Xibo.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Http;
 // ! ==================== Consuming XIBO  ==================== 
 
 Route::get('/get-displays-data', function (Request $request) {
+
+
+    Display::truncate() ; 
+    Layout::truncate() ; 
+    // TODO : this truncate is not suitable at all , TEMPORARY 
     //! Below is Logic For SYNCING "Displays&&LAyOUTS"+TOken
     /**
      * 1- Get the Token : 
